2e66e2e50dce01eb02273f5347beedf82ef7399e
ChuchoAraujo <jeseazuara@gmail.com>
2023-04-26 10:17:19 +0000

    routes login y bregister

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,51 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    firstname = db.Column(db.String(20), nullable=False)
+    lastname = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"), nullable=False)
+    role = db.relationship("Roles", backref="users")
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<{self.username}>'
+
+    def __init__(self, username, firstname, lastname, email, password, role_id):
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.password = password
+        self.role_id = role_id
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname, 
+            "lastname": self.lastname, 
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "role": self.role.serialize()
         }
+
+class Roles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<{self.type}>'
+
+    def __init__(self, type):
+        self.type = type
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type
+        }  
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,79 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Roles
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from werkzeug.security import generate_password_hash, check_password_hash
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.serialize() for user in users]), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 
-    return jsonify(response_body), 200
+@api.route("/register", methods=["POST"])
+def create_new_user():
+    data = request.get_json()
+    role_name = data.get('role')
+
+    # Verificar si el usuario ya existe
+    existing_user = User.query.filter_by(username=data['username']).first()
+    if existing_user:
+        return jsonify({'message': 'El usuario ya existe'}), 400
+
+    # Encriptar contraseña
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+
+    # Obtener el objeto Role correspondiente al nombre recibido
+    role = Roles.query.filter_by(type=role_name).first()
+    if not role:
+        return jsonify({'message': 'Rol no encontrado'}), 400
+
+    # Crear el nuevo usuario con la relación al rol
+    new_user = User(
+        username=data['username'],
+        firstname=data['firstname'],
+        lastname=data['lastname'],
+        email=data['email'],
+        password=hashed_password,
+        role_id=role.id
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'user': new_user.serialize()}), 200
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    # Obtener los datos del usuario desde el cliente
+    data = request.get_json()
+    role_name = data.get('role')
+    username = data.get('username')
+    password = data.get('password')
+
+    # Verificar si el usuario existe
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({'message': 'Usuario o contraseña incorrectos'}), 401
+
+    # Verificar si la contraseña es correcta
+    if not check_password_hash(user.password, password):
+        return jsonify({'message': 'Usuario o contraseña incorrectos'}), 401
+
+    # Verificar si el role es correcto
+    if user.role.type != role_name:
+        return jsonify({'message': 'Rol incorrecto'}), 401
+
+    # Generar un token JWT y devolverlo como respuesta
+    access_token = create_access_token(identity=username)
+    return jsonify({'access_token': access_token}), 200
+
+    
